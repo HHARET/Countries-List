edf01a231178b991fa47a64a3acdcff5a8ff0b15
Tarek <tarek_s_a@yahoo.com>
2016-07-20 10:19:08 -0430

    json file modified double quotes removed from population value

--- country-list/countryListCtrl.js
+++ country-list/countryListCtrl.js
@@ -3,7 +3,7 @@ angular
 	.module('countriesApp')
     .controller('countryListCtrl', function ($scope,getJSON){
         getJSON.list(function(countries){
-          $scope.countriesList=countries;
+					  $scope.countriesList=countries;
         });
         $scope.sortField="population";
 				});

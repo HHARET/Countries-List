ea5d1147b483ad942b93422aad022032e935a98f
Tarek <tarek_s_a@yahoo.com>
2016-07-23 10:12:17 -0430

    double check

--- app/country-list/countryListCtrl.js
+++ app/country-list/countryListCtrl.js
@@ -1,11 +1,11 @@
 'use strict';
 angular
 	.module('countriesApp')
-    .controller('countryListCtrl', function ($scope,getJSON){
+    .controller('countryListCtrl', function (getJSON,$scope){
         getJSON.list(function(countries){
 					  $scope.countriesList=countries;
         });
-        $scope.sortField="population";
+      $scope.sortField="population";
 
 				})
 				.directive("continent-list",function(){
